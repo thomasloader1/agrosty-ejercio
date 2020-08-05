52e3faab577d4702843d9079048b692c187c5416
Gomez Tomas Gonzalo <tomas.gomez@aufieroinformatica.com.ar>
2020-08-04 23:27:42 -0300

    Optimizacion y estilizacion

--- js/main.js
+++ js/main.js
@@ -16,8 +16,10 @@ function login() {
         if (user.value === "" && pass.value === "") {
             alertErr.innerHTML = "Debe ingresar Usuario y Password";
             alertErr.classList.replace("d-none", "d-block");
-            user.style.border = "2px solid red";
-            pass.style.border = "2px solid red";
+            user.style.boxShadow = "0 1px 0 0 red";
+            pass.style.boxShadow = "0 1px 0 0 red";
+            $("#user-icon").css('color', 'red');
+            $("#password-icon").css('color', 'red');
         }
         else {
             window.location.href = e.target.getAttribute("href");
@@ -26,26 +28,32 @@ function login() {
 
     user.addEventListener('blur', ()=>{
         if(user.value === ""){
-            user.style.border = "2px solid red";
+            user.style.boxShadow = "0 1px 0 0 red";
+            $("#user-icon").css('color', 'red');
             alertErr.innerHTML = "Debe ingresar Usuario"
             alertErr.classList.replace("d-none", "d-block")
         }else{
-            user.style.border = "1px solid green";
+            user.style.boxShadow = "0 1px 0 0 green"
+            $("#user-icon").css('color', 'green');
             alertErr.classList.replace("d-block", "d-none")
         }
     })
     
     pass.addEventListener('blur', ()=>{
         if(pass.value === "" && user.value === ""){
-            pass.style.border = "2px solid red";
+            pass.style.boxShadow = "0 1px 0 0 red";
+            $("#password-icon").css('color', 'red');
             alertErr.innerHTML = "Debe ingresar Usuario y Password"
             alertErr.classList.replace("d-none", "d-block")
         }else if(pass.value === "" ){
-            pass.style.border = "2px solid red";
+            pass.style.boxShadow = "0 1px 0 0 red";
+            $("#password-icon").css('color', 'red');
             alertErr.innerHTML = "Debe ingresar password"
             alertErr.classList.replace("d-none", "d-block")
         }else{
-            pass.style.border = "1px solid green";
+            pass.style.boxShadow = "0 1px 0 0 green"
+            $("#user-icon").css('color', 'green');
+            $("#password-icon").css('color', 'green');
             alertErr.classList.replace("d-block", "d-none")
         }
     })
